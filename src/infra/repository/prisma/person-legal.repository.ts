5f9cd04cf10594ccff6b424d770fe2aa2fde4a2f
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-06-21 18:42:22 -0300

    Created repository to legal person

---
+++ src/infra/repository/prisma/person-legal.repository.ts
@@ -0,0 +1,37 @@
+import { type PersonLegalEntity } from '@/core/entities/person-legal.entity'
+import { type PersonLegalRepositoryPort } from '@/infra/port/person-legal.port'
+import { Result } from 'true-myth'
+import { Connection } from './connection'
+
+export class PersonLegalPrismaRepository implements PersonLegalRepositoryPort {
+  async create (person: PersonLegalEntity): Promise<Result<boolean, Error>> {
+    try {
+      await Connection.person.create({
+        data: {
+          id: person.id,
+          name: person.name,
+          email: person.email,
+          cpf: person.cpf,
+          cnpj: person.cnpj,
+          address_line1: person.addressLine1,
+          address_line2: person.addressLine2,
+          address_numer: person.addressNumber,
+          city: person.city,
+          neighborhood: person.neighborhood,
+          contract_read: Number(person.contractRead),
+          zipcode: person.zipcode,
+          state: person.state,
+          person_type: person.personType,
+          phone: {
+            createMany: {
+              data: person.phone
+            }
+          }
+        }
+      })
+      return Result.ok(true)
+    } catch (error: any) {
+      return Result.err(error)
+    }
+  }
+}
