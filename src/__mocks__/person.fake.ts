50f729c4509fb4da3422e9e8ad82da9a7f053a3e
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-06-21 15:01:31 -0300

    Refactory personFake to create a new object from PersonEntity

--- src/__mocks__/person.fake.ts
+++ src/__mocks__/person.fake.ts
@@ -1,10 +1,32 @@
-import { faker } from "@faker-js/faker";
+import { type AddressEntity } from '@/entities/addess.entity'
+import { PersonNaturalEntity } from '@/entities/person-natural.entity'
+import { type PersonEntity } from '@/entities/person.entity'
+import { type PhoneEntity } from '@/entities/phone.entity'
+import { faker } from '@faker-js/faker'
 
-export const PersonFake = {
-     name: faker.person.fullName(),
-     cellPhone: faker.phone.number(),
-     phone: faker.phone.number(),
-     cpf: faker.number.int({ min: 11, max: 11 }).toString(),
-     email: faker.internet.email(),
-     readContract: true
+const phone: PhoneEntity [] = [{
+  number: faker.phone.number(),
+  type: 'cell-phone'
+},
+{
+  number: faker.phone.number(),
+  type: 'phone'
+}]
+
+const person: PersonEntity = {
+  name: faker.person.fullName(),
+  email: faker.internet.email()
 }
+const document = faker.number.int({ min: 11, max: 11 }).toString()
+
+const address: Omit<AddressEntity, 'personId'> = {
+  addressLine1: faker.location.streetAddress(),
+  addressLine2: faker.location.street(),
+  addressNumber: Number(faker.location.buildingNumber()),
+  city: faker.location.city(),
+  state: faker.location.state(),
+  zipcode: faker.location.zipCode(),
+  neighborhood: faker.location.street()
+}
+
+export const PersonNaturalFake = new PersonNaturalEntity(person, document, address, phone)
