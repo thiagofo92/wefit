1641a4c323956f615c3e0bf8861ee779f69ed464
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-06-21 10:45:04 -0300

    Add config file to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+const path = resolve(__dirname)
+
+export default defineConfig({
+  test: {
+    coverage: {
+      functions: 80,
+      branches: 80,
+      statements: 80,
+      lines: 80
+    }
+  },
+  resolve: {
+    alias: {
+      '@': `${path}/src`,
+      '@test': `${path}/test`
+    }
+  }
+})
