328a8ed7f953193158bd74e147280ec6523be99e
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-06-22 10:35:26 -0300

    Update

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,9 @@
+import { Logger } from '@/shared/logs/logger'
 import { StartServer } from './api/express'
+
+process.on('SIGTERM', () => {
+  Logger.warn('Server was shutdown')
+  process.exit()
+})
 
 StartServer()
